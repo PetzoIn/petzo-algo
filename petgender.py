1bc4810ad44fdf5f7ffe295820bfa4b2ff60a0c2
Siddharth Goyal <siddharth.game@gmail.com>
2017-07-03 18:51:09 +0530

    a Removed redundant files

--- petgender.py
+++
@@ -1,3 +0,0 @@
-def gender(gen, pregnant=None, neutered=None):
-	# returning a tuple
-	return gen, pregnant,neutered
--- weight.py
+++
@@ -1,5 +0,0 @@
-import math
-
-def weight(kgs):
-	rer = 70 * (kgs**0.75)
-	return rer
