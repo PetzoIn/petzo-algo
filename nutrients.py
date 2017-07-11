0d32d2cd336dbf1f2c780269c376c1ae09adb885
rajat2797 <rajatsinghal1997@gmail.com>
2017-07-11 17:22:55 +0530

    nutrients

--- nutrients.py
+++ nutrients.py
@@ -1,6 +1,11 @@
 import csv
 
-def nutrientsComposition(age, healthIssue, skin, stoolConsistency, unusualSubstance):
+# age = GROWTH/ ADULT/ OLD (TEXT)
+# healthIssue = OPTIONS (TEXT)
+# skinCoat = ROUGH/SHINY/FAIR (TEXT)
+# stoolConsistency = NORMAL/LOOSE/HARD (TEXT)
+# unusualSubstance = OPTIONS (TEXT)
+def nutrientsComposition(age, healthIssue, skinCoat, stoolConsistency, unusualSubstance):
 	nutrientsIndex = 0
 	ageIndex = -1
 	healthIssueIndex = -1
@@ -57,30 +62,30 @@ def nutrientsComposition(age, healthIssue, skin, stoolConsistency, unusualSubsta
 				values.append(row[ageIndex])
 
 		# SKIN
-		if skin.lower() == 'shiny':
+		if skinCoat.lower() == 'shiny':
 			pass
 
-		elif skin.lower() == 'rough':
+		elif skinCoat.lower() == 'rough':
 			for i in len(n):
 				if 'Vitamin A' in n[i]:
-					values[i] = values[i]*()
+					values[i] = values[i]*(1.20)
 
 				elif 'Zinc' in n[i]:
-					values[i] = values[i]*()
+					values[i] = values[i]*(1.25)
 
 				elif 'Linoleic' in n[i]:
-					values[i] = values[i]*()
+					values[i] = values[i]*(1.25)
 
-		elif skin.lower() == 'fair':
+		elif skinCoat.lower() == 'fair':
 			for i in len(n):
 				if 'Vitamin A' in n[i]:
-					values[i] = values[i]*()
+					values[i] = values[i]*(1.20)
 
 				elif 'Choline' in n[i]:
-					values[i] = values[i]*()
+					values[i] = values[i]*(1.10)
 
 				elif 'Linoleic' in n[i]:
-					values[i] = values[i]*()
+					values[i] = values[i]*(1.25)
 
 		# STOOLCONSISTENCY
 		if stoolConsistency.lower() == 'normal':
@@ -89,50 +94,50 @@ def nutrientsComposition(age, healthIssue, skin, stoolConsistency, unusualSubsta
 		elif stoolConsistency.lower() == 'loose':
 			for i in len(n):
 				if 'Vitamin A' in n[i]:
-					values[i] = values[i]*()
+					values[i] = values[i]*(1.20)
 
 				elif 'Fiber' in n[i]:
-					values[i] = values[i]*()
+					values[i] = values[i]*(0.80)
 
 				elif 'Fat' in n[i]:
-					values[i] = values[i]*()
+					values[i] = values[i]*(0.80)
 
 		elif stoolConsistency.lower() == 'hard':
 			for i in len(n):
 				if 'Vitamin A' in n[i]:
-					values[i] = values[i]*()
+					values[i] = values[i]*(1.20)
 
 				elif 'Vitamin E' in n[i]:
-					values[i] = values[i]*()
+					values[i] = values[i]*(1.30)
 
 				elif 'Fiber' in n[i]:
-					values[i] = values[i]*()
+					values[i] = values[i]*(1.20)
 
 				elif 'Fat' in n[i]:
-					values[i] = values[i]*()
+					values[i] = values[i]*(1.20)
 
 		# UNUSUALSUBSTANCE
 		if 'grass' in unusualSubstance.lower():
 			for i in len(n):
 				if 'Vitamin A' in n[i]:
-					values[i] = values[i]*()
+					values[i] = values[i]*(1.20)
 
 				elif 'Fiber' in n[i]:
-					values[i] = values[i]*()
+					values[i] = values[i]*(1.20)
 
 		elif 'litter' in unusualSubstance.lower():
 			for i in len(n):
 				if 'Thiamine' in n[i]:
-					values[i] = values[i]*()
+					values[i] = values[i]*(1.10)
 
 				elif 'Riboflavin' in n[i]:
-					values[i] = values[i]*()
+					values[i] = values[i]*(1.10)
 
 				elif 'Niacin' in n[i]:
-					values[i] = values[i]*()
+					values[i] = values[i]*(1.20)
 
 				elif 'Pyridoxine' in n[i]:
-					values[i] = values[i]*()
+					values[i] = values[i]*(1.10)
 
 		elif 'mud' in unusualSubstance.lower():
 			for i in range(8,20):
@@ -142,23 +147,23 @@ def nutrientsComposition(age, healthIssue, skin, stoolConsistency, unusualSubsta
 			for i in range(20,28):
 				values[i] = values[i]*(1.15)
 
-		elif 'grass' in unusualSubstance.lower():
+		elif 'cloth' in unusualSubstance.lower():
 			for i in len(n):
 				if 'Vitamin A' in n[i]:
-					values[i] = values[i]*()
+					values[i] = values[i]*(1.20)
 
 				elif 'Fiber' in n[i]:
-					values[i] = values[i]*()
+					values[i] = values[i]*(1.10)
 
 		elif 'floor' in unusualSubstance.lower():
 			for i in len(n):
 				if 'Calcium' in n[i]:
-					values[i] = values[i]*()
+					values[i] = values[i]*(1.20)
 
 				elif 'Phosphorus' in n[i]:
-					values[i] = values[i]*()
+					values[i] = values[i]*(1.20)
 
 				elif 'Magnesium' in n[i]:
-					values[i] = values[i]*()
+					values[i] = values[i]*(1.10)
 
 		return values
